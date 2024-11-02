bb1521401927ba615476371179413d157304066e
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-02 00:47:25 -0500

    Conexion base de datos

--- Desarrollo/MentalSync/Fuentes/server/src/db.js
+++ Desarrollo/MentalSync/Fuentes/server/src/db.js
@@ -1,14 +1,18 @@
 import pgPromise from "pg-promise";
-import { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } from "./config.js";
+import { DB_URL } from "./config.js";
 
-const pgp = pgPromise({});
+const pgp = pgPromise({
+  error: (error, e) => {
+    console.error('Database error:', error);
+  }
+});
 
 const dbConfig = {
-  host: DB_HOST,
-  port: DB_PORT,
-  database: DB_NAME,
-  user: DB_USER,
-  password: DB_PASSWORD,
+  connectionString: DB_URL,
+  ssl: { rejectUnauthorized: false },
+  max: 10,
+  idleTimeoutMillis: 30000,
+  keepAlive: true
 };
 
 const db = pgp(dbConfig);
