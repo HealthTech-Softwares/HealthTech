e38c1f105be52b94f49db6ce23d1590e10b72083
RichardTorresRivera <rmtr2711@gmail.com>
2024-11-02 10:13:48 -0500

    conexion a la db al 100

--- Desarrollo/MentalSync/Fuentes/server/src/config.js
+++ Desarrollo/MentalSync/Fuentes/server/src/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export const PORT = 4000;
 export const DB_HOST = process.env.DB_HOST;
+export const DB_URL = process.env.DB_URL;
 export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
