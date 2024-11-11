ab9bed116a8230ab001006fde40991fec324822e
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-11-11 16:08:12 -0500

    mis pacientes lista psico reestructura

--- Desarrollo/MentalSync/Fuentes/client/src/components/mis_pacientes/mis_pacientes.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/components/mis_pacientes/mis_pacientes.jsx
@@ -1,40 +1,38 @@
-import styles from './mis_pacientes.module.css';
-import logoMental from '../../assets/logo_mentalsync.png';
-import logoUsuario from '../../assets/user.png';
+import mis from './mis_pacientes.module.css';
+import styles from '../principales.module.css';
+import { BotonAccion, InputInfoSinLabel, NavBarMental, NombrePantalla, PacienteConFoto } from '../principales';
+
+export function SelectEstado(){
+    return(
+        <select className="form-select">
+            <option value="1">Estado</option>
+            <option value="2">En proceso</option>
+            <option value="3">Solo cita</option>
+            <option value="4">Completado</option>
+        </select>
+    )
+}
 
 export function MisPacientes(){
     return(
         <body className={`${styles.fondo}`}>
-            <nav class="navbar bg-body-tertiary">
-                <div class="container-fluid d-flex align-items-center">
-                    <a class="navbar-brand d-flex align-items-center" href="#">
-                        <img src={logoMental} alt="Logo" class={`d-inline-block align-text-top ${styles.logoNav}`}/>
-                    MentalSync
-                    </a>
-                    <img src={logoUsuario} alt="Usuario" class={`${styles.logoNav}`} />
-                </div>
-            </nav>
+            <NavBarMental/>
 
             <section>
                 <div className="container-fluid">
                     <div className="row ms-4">
                         <div className="col-12">
-                            <h2 className='mb-3 mt-3'>Mis pacientes</h2>
+                            <NombrePantalla nombre="Mis pacientes"/>
                             <form>
                                 <div className="row mb-4">
                                     <div className="col-3">
-                                        <input type="text" className="form-control" placeholder="Código/Nombre"/>
+                                        <InputInfoSinLabel/>
                                     </div>
                                     <div className="col-3">
-                                        <select className="form-select">
-                                            <option value="1">Estado</option>
-                                            <option value="2">En proceso</option>
-                                            <option value="3">Solo cita</option>
-                                            <option value="4">Completado</option>
-                                        </select>
+                                        <SelectEstado/>
                                     </div>
                                     <div className="col-2">
-                                        <button type="submit" className="btn btn-primary">Buscar</button>
+                                        <BotonAccion nombre="Buscar"/>
                                     </div>
                                 </div>
                             </form>
@@ -42,60 +40,48 @@ export function MisPacientes(){
                     </div>
                     <div className={`row justify-content-center`}>
                         <div className="col-3">
-                            <div class={`card ${styles.myCard} mb-3`}>
+                            <div class={`card ${mis.myCard} mb-3`}>
                                 <div class="card-body">
                                     <div className="row align-items-center">
                                         <div className="col-12 text-center">
-                                            <img src={logoUsuario} alt="Psicólogo" className={`mb-3 ${styles.logoPsico}`} />
-                                            <h5 class="card-title">Santos Jiggets</h5>
-                                            <h6 class="card-subtitle mb-2 text-body-secondary">ID: U0304</h6>
-                                            <p>Última cita: 23/09/2024</p>
-                                            <button className='btn btn-primary text-center'>Generar diagnóstico</button>
+                                            <PacienteConFoto nombre="Santos Jiggets" identificador="ID: U0304" ultimaCita="23/09/2024"/>
+                                            <BotonAccion nombre="Generar diagnóstico"/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-3">
-                            <div class={`card ${styles.myCard} mb-3`}>
+                            <div class={`card ${mis.myCard} mb-3`}>
                                 <div class="card-body">
                                     <div className="row align-items-center">
                                         <div className="col-12 text-center">
-                                            <img src={logoUsuario} alt="Psicólogo" className={`mb-3 ${styles.logoPsico}`} />
-                                            <h5 class="card-title">Olivia Zacarias</h5>
-                                            <h6 class="card-subtitle mb-2 text-body-secondary">ID: U0215</h6>
-                                            <p>Última cita: 17/07/2024</p>
-                                            <button className='btn btn-primary text-center'>Ver historia clínica</button>
+                                            <PacienteConFoto nombre="Olivia Zacarias" identificador="ID: U0215" ultimaCita="17/07/2024"/>
+                                            <BotonAccion nombre="Ver historia clínica"/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-3">
-                            <div class={`card ${styles.myCard} mb-3`}>
+                            <div class={`card ${mis.myCard} mb-3`}>
                                 <div class="card-body">
                                     <div className="row align-items-center">
                                         <div className="col-12 text-center">
-                                            <img src={logoUsuario} alt="Psicólogo" className={`mb-3 ${styles.logoPsico}`} />
-                                            <h5 class="card-title">Charlotte Abonza</h5>
-                                            <h6 class="card-subtitle mb-2 text-body-secondary">ID: U0321</h6>
-                                            <p>Última cita: 20/06/2024</p>
-                                            <button className='btn btn-primary text-center'>Ver historia clínica</button>
+                                            <PacienteConFoto nombre="Charlotte Abonza" identificador="ID: U0321" ultimaCita="20/06/2024"/>
+                                            <BotonAccion nombre="Ver historia clínica"/>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div className="col-3">
-                            <div class={`card ${styles.myCard} mb-3`}>
+                            <div class={`card ${mis.myCard} mb-3`}>
                                 <div class="card-body">
                                     <div className="row align-items-center">
                                         <div className="col-12 text-center">
-                                            <img src={logoUsuario} alt="Psicólogo" className={`mb-3 ${styles.logoPsico}`} />
-                                            <h5 class="card-title">Daniel Shivley</h5>
-                                            <h6 class="card-subtitle mb-2 text-body-secondary">ID: U0123</h6>
-                                            <p>Última cita: 14/02/2024</p>
-                                            <button className='btn btn-primary text-center'>Ver historia clínica</button>
+                                            <PacienteConFoto nombre="Daniel Shivley" identificador="ID: U0123" ultimaCita="14/02/2024"/>
+                                            <BotonAccion nombre="Ver historia clínica"/>
                                         </div>
                                     </div>
                                 </div>
