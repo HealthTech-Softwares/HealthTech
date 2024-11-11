213ceb47d4a5a3b63f740ba0769e0d8d2cf69e32
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-11-11 10:00:48 -0500

    generar diagnostico pantalla html css

--- Desarrollo/MentalSync/Fuentes/client/src/App.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/App.jsx
@@ -8,6 +8,7 @@ import { MetodoPago} from './components/metodo_pago/metodo_pago'
 import { CalificarPsico } from './components/calificar_psico/calificar_psico'
 import { MisCitas } from './components/mis_citas/mis_citas'
 import { CitaVirtual } from './components/cita_virtual/cita_virtual'
+import { GenerarDiagnostico } from './components/generar_diagnostico/generar_diagnostico'
 
 
 function App() {
@@ -16,13 +17,21 @@ function App() {
   return (
     <>
       <Routes>
+        {/* Pantalla Inicial */}
         <Route path="/" element={<Inicio/>}/>
+
+        {/* Pantallas Paciente */}
         <Route path="/principal" element={<BuscarPsico/>}/>
         <Route path="/reserva-cita" element={<ReservaCita/>}/>
         <Route path="/metodo-pago" element={<MetodoPago/>}/>
         <Route path="/calificar-psicologo" element={<CalificarPsico/>}/>
         <Route path="/mis-citas" element={<MisCitas/>}/>
-        <Route path="/cita-reservada" element={<CitaVirtual/>}/> 
+        <Route path="/cita-reservada" element={<CitaVirtual/>}/>
+
+        {/* Pantallas Psicólogo */}
+        <Route path='/generar-diagnostico' element={<GenerarDiagnostico/>}/>
+
+        {/* Pantallas Administrador */}
         <Route path="*" element={<Inicio/>}/>
       </Routes>
     </>
