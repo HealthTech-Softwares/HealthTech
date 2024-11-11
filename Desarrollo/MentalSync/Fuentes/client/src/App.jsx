c7bca8bd91108fc66573aae6ca2f39b0200703df
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-11-10 23:10:51 -0500

    metodo pago pantalla html css

--- Desarrollo/MentalSync/Fuentes/client/src/App.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import { BuscarPsico } from './components/pantalla_principal_paciente/buscar_psico'
 import { Inicio } from './components/inicio_sesion/inicio'
 import { ReservaCita } from './components/reservar_cita/reservar_cita'
+import { MetodoPago } from './components/metodo_pago/metodo_pago'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +11,7 @@ function App() {
   return (
     <>
       <div>
-        <ReservaCita/>
+        <MetodoPago/>
       </div>
     </>
   )
