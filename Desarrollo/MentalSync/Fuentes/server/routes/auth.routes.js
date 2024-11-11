6a8c8f847fe186ef330b585361fd23f8bc260282
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-11 16:12:49 -0500

    login y autenticacion

---
+++ Desarrollo/MentalSync/Fuentes/server/routes/auth.routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { login, logout } from '../controllers/auth.controller.js';
+
+const router = Router();
+
+router.post("/login", login);
+router.post("/logout", logout);
+
+export default router;
