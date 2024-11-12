a3e9d37e531a8958511947d47bddecbf26837b85
Jeremy <jeremyjr02@outlook.com>
2024-11-12 01:08:47 -0500

    Update turnos.routes.js

--- Desarrollo/MentalSync/Fuentes/server/routes/turnos.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/turnos.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { createTurnoPsicologo, getTurnoPsicologo, getTurnos, getTurnosFaltantesPsicologo } from "../controllers/turnos.controller.js";
+import {
+  createTurnoPsicologo,
+  getTurnoPsicologo,
+  getTurnos,
+  getTurnosFaltantesPsicologo,
+} from "../controllers/turnos.controller.js";
 import { verifyToken, isAdmin } from "../libs/auth.middleware.js";
 
 const router = Router();
@@ -7,6 +12,11 @@ const router = Router();
 router.post("/turnos/:id", verifyToken, isAdmin, createTurnoPsicologo);
 router.get("/turnos", verifyToken, isAdmin, getTurnos);
 router.get("/turnos/:id", verifyToken, isAdmin, getTurnoPsicologo);
-router.get("/turnos/faltantes/:id", verifyToken, isAdmin, getTurnosFaltantesPsicologo);
+router.get(
+  "/turnos/faltantes/:id",
+  verifyToken,
+  isAdmin,
+  getTurnosFaltantesPsicologo
+);
 
 export default router;
