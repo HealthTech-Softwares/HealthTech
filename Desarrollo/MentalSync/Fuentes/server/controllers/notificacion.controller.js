92e167d1811101a243be8af6d266fa86f252ab7a
Jeremy <jeremyjr02@outlook.com>
2024-11-12 00:57:47 -0500

    Create Notificacion

---
+++ Desarrollo/MentalSync/Fuentes/server/controllers/notificacion.controller.js
@@ -0,0 +1,16 @@
+import db from "../db.js";
+
+export const createNotificacion = async (req, res, next) => {
+  try {
+    const { idemisor, idreceptor, mensaje } = req.body;
+
+    const result = await db.one(
+      `INSERT INTO notificacion (idemisor, idreceptor, mensaje) VALUES ($1, $2, $3) RETURNING *`,
+      [idemisor, idreceptor, mensaje]
+    );
+
+    return res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
