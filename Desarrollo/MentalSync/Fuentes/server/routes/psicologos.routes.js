bf487d9541cf81f0a7dc20d796e2f863c9b54728
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-12 01:47:09 -0500

    rutas (descripcion)

--- Desarrollo/MentalSync/Fuentes/server/routes/psicologos.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/psicologos.routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 import { createPsicologo, deletePsicologo, getPsicologo, getPsicologos, updatePsicologo } from "../controllers/psicologos.controller.js";
-import { verifyToken, isAdmin, isPsicologo } from "../libs/auth.middleware.js";
+import { verifyToken, isAdmin, isPaciente, isPsicologo } from "../libs/auth.middleware.js";
 
 const router = Router();
 
-router.post("/psicologos", verifyToken, isAdmin, createPsicologo);
-router.get("/psicologos", verifyToken, isAdmin, getPsicologos);
-router.get("/psicologo/:id", verifyToken, isAdmin, getPsicologo);
-router.get("/psicologo/perfil", verifyToken, isPsicologo, getPsicologo);
-router.delete("/psicologos/:id", verifyToken, isAdmin, deletePsicologo);
-router.put("/psicologos", verifyToken, isPsicologo, updatePsicologo);
+router.post("/psicologos", verifyToken, isAdmin, createPsicologo); //Crear psicologo por el admin
+router.get("/psicologos", verifyToken, isAdmin, isPaciente, getPsicologos); //Ver todos los psicologos
+router.get("/psicologo/:id", verifyToken, isAdmin, getPsicologo); //Ver psicologo especifico
+router.get("/psicologo/perfil", verifyToken, isPsicologo, getPsicologo); //Ver el psicologo, mientras esta logeado
+router.delete("/psicologos/:id", verifyToken, isAdmin, deletePsicologo); //Elimnar psicologo por el admin
+router.put("/psicologos", verifyToken, isPsicologo, updatePsicologo); //Actualizar datos mientras esta logeado
 
 export default router;
--- Desarrollo/MentalSync/Fuentes/server/routes/turnos.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/turnos.routes.js
@@ -4,9 +4,9 @@ import { verifyToken, isAdmin } from "../libs/auth.middleware.js";
 
 const router = Router();
 
-router.post("/turnos/:id", verifyToken, isAdmin, createTurnoPsicologo);
-router.get("/turnos", verifyToken, isAdmin, getTurnos);
-router.get("/turnos/:id", verifyToken, isAdmin, getTurnoPsicologo);
-router.get("/turnos/faltantes/:id", verifyToken, isAdmin, getTurnosFaltantesPsicologo);
+router.post("/turnos/:id", verifyToken, isAdmin, createTurnoPsicologo); //Asignar turno a psicologo
+router.get("/turnos", verifyToken, isAdmin, getTurnos); //Ver todos los turnos
+router.get("/turnos/:id", verifyToken, isAdmin, getTurnoPsicologo); //Ver los turnos de un psicologo
+router.get("/turnos/faltantes/:id", verifyToken, isAdmin, getTurnosFaltantesPsicologo); //Ver los turnos que no tiene un psicologo
 
 export default router;
