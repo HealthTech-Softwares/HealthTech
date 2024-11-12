30441d15fbcd5d2451973d961495923c57b0a68d
Jeremy <jeremyjr02@outlook.com>
2024-11-12 00:58:42 -0500

    Get Todas Notificaciones

--- Desarrollo/MentalSync/Fuentes/server/controllers/notificacion.controller.js
+++ Desarrollo/MentalSync/Fuentes/server/controllers/notificacion.controller.js
@@ -14,3 +14,15 @@ export const createNotificacion = async (req, res, next) => {
     next(error);
   }
 };
+
+export const getTodasNotificaciones = async (res, next) => {
+  try {
+    const result = await db.any(
+      `SELECT *
+      FROM notificacion`
+    );
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
+}
