c3e5260d0f72c0cc0d479fef7180d368ec9bb0a7
Jeremy <jeremyjr02@outlook.com>
2024-11-12 01:08:23 -0500

    Update especialidad.routes.js

--- Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
@@ -1,5 +1,10 @@
 import { Router } from "express";
-import { createEspPsicologo, getEspecialidades, getEspsPsicologo, getEspFaltantesPsicologo, } from "../controllers/especialidad.controller.js";
+import {
+  createEspPsicologo,
+  getEspecialidades,
+  getEspsPsicologo,
+  getEspFaltantesPsicologo,
+} from "../controllers/especialidad.controller.js";
 import { verifyToken, isAdmin } from "../libs/auth.middleware.js";
 
 const router = Router();
@@ -7,6 +12,11 @@ const router = Router();
 router.post("/especialidades/:id", verifyToken, isAdmin, createEspPsicologo);
 router.get("/especialidades", verifyToken, isAdmin, getEspecialidades);
 router.get("/especialidades/:id", verifyToken, isAdmin, getEspsPsicologo);
-router.get("/especialidades/faltantes/:id", verifyToken, isAdmin, getEspFaltantesPsicologo);
+router.get(
+  "/especialidades/faltantes/:id",
+  verifyToken,
+  isAdmin,
+  getEspFaltantesPsicologo
+);
 
 export default router;
