413e6baaa8f43e64687a59e31b292ca2d69a6f4b
Jeremy <jeremyjr02@outlook.com>
2024-11-14 23:18:19 -0500

    Update App.jsx

--- Desarrollo/MentalSync/Fuentes/client/src/App.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/App.jsx
@@ -1,23 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { Inicio } from './components/inicio_sesion/inicio'
-import { BuscarPsico } from './components/pantalla_principal_paciente/buscar_psico'
-import { ReservaCita } from './components/reservar_cita/reservar_cita'
-import { MetodoPago} from './components/metodo_pago/metodo_pago'
-import { CalificarPsico } from './components/calificar_psico/calificar_psico'
-import { MisCitas } from './components/mis_citas/mis_citas'
-import { CitaVirtual } from './components/cita_virtual/cita_virtual'
-import { GenerarDiagnostico } from './components/generar_diagnostico/generar_diagnostico'
-import { ModificarDatosPaciente } from './components/modificar_datos_paciente/modificar_datos_paciente'
-import { ModificarDatosPsicologo } from './components/modificar_datos_psicologo/modificar_datos_psicologo'
-import { MisPacientes } from './components/mis_pacientes/mis_pacientes'
-import { ListaPsicologos } from './components/lista_psicologos/lista_psicologos'
-import { HistoriaClinica } from './components/historia_clinica/historia_clinica'
-import { Notificaciones } from './components/notificaciones/notificaciones'
-import { AgregarPsicologo } from './components/agregar_psicologo/agregar_psicologo'
-import { CrearCuenta } from './components/inicio_sesion/crear_cuenta'
-
+import { useState } from "react";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Inicio } from "./components/inicio_sesion/inicio";
+import { BuscarPsico } from "./components/pantalla_principal_paciente/buscar_psico";
+import { ReservaCita } from "./components/reservar_cita/reservar_cita";
+import { MetodoPago } from "./components/metodo_pago/metodo_pago";
+import { CalificarPsico } from "./components/calificar_psico/calificar_psico";
+import { MisCitas } from "./components/mis_citas/mis_citas";
+import { CitaVirtual } from "./components/cita_virtual/cita_virtual";
+import { GenerarDiagnostico } from "./components/generar_diagnostico/generar_diagnostico";
+import { ModificarDatosPaciente } from "./components/modificar_datos_paciente/modificar_datos_paciente";
+import { ModificarDatosPsicologo } from "./components/modificar_datos_psicologo/modificar_datos_psicologo";
+import { MisPacientes } from "./components/mis_pacientes/mis_pacientes";
+import { ListaPsicologos } from "./components/lista_psicologos/lista_psicologos";
+import { HistoriaClinica } from "./components/historia_clinica/historia_clinica";
+import { Notificaciones } from "./components/notificaciones/notificaciones";
+import { AgregarPsicologo } from "./components/agregar_psicologo/agregar_psicologo";
+import { CrearCuenta } from "./components/inicio_sesion/crear_cuenta";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,10 +25,10 @@ function App() {
     <>
       <Routes>
         {/* Pantalla Inicial */}
-        <Route path="/" element={<Inicio/>}/>
-        <Route path='/crear-cuenta' element={<CrearCuenta/>}/>
-        <Route path="*" element={<Inicio/>}/>
-        <Route path="/notificaciones" element={<Notificaciones/>}/>
+        <Route path="/" element={<Inicio />} />
+        <Route path="/crear-cuenta" element={<CrearCuenta />} />
+        <Route path="*" element={<Inicio />} />
+        <Route path="/notificaciones" element={<Notificaciones />} />
 
         {/* Pantallas Paciente */}
         <Route path="/principal" element={<BuscarPsico />} />
@@ -53,9 +52,8 @@ function App() {
         <Route path="/historia-clinica" element={<HistoriaClinica />} />
 
         {/* Pantallas Administrador */}
-        <Route path='/lista-psicologos' element={<ListaPsicologos/>}/>
-        <Route path='/agregar-psicologo' element={<AgregarPsicologo/>}/>
-        
+        <Route path="/lista-psicologos" element={<ListaPsicologos />} />
+        <Route path="/agregar-psicologo" element={<AgregarPsicologo />} />
       </Routes>
     </>
   );
