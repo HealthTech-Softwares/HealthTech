559c5ed068de158a2ce7e5b6e490737f3a6b3440
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-20 00:36:41 -0500

    rutas

--- Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
@@ -5,7 +5,8 @@ import { verifyToken, isAdmin, isPaciente } from "../libs/auth.middleware.js";
 const router = Router();
 
 router.post("/especialidades/:id", verifyToken, isAdmin, createEspPsicologo); //Asignar especialidad a psicologo
-router.get("/especialidades", verifyToken, isAdmin, isPaciente, getEspecialidades); //Ver las especialidades que tiene el psicologo
+router.get("/especialidades", verifyToken, isAdmin, getEspecialidades); //Ver las especialidades que tiene el psicologo (para el admin)
+router.get("/especialidades", verifyToken, isPaciente, getEspecialidades); //Ver las especialidades que tiene el psicologo (para el paciente)
 router.get("/especialidades/:id", verifyToken, isAdmin, getEspsPsicologo); //Ver las especialidades que tiene un psicologo
 router.get("/especialidades/faltantes/:id", verifyToken, isAdmin, getEspFaltantesPsicologo); //Ver las especialidades que no tiene un psicologo
 
