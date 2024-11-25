3ba61da693cfa31714750fe2de7291d7b2c71b0e
AcherPlus <enmanuel.obando@unmsm.edu.pe>
2024-11-25 15:58:22 -0500

    errores html buscar psico reservar cita

--- Desarrollo/MentalSync/Fuentes/client/src/components/paciente/reservar_cita/reservar_cita.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/components/paciente/reservar_cita/reservar_cita.jsx
@@ -13,23 +13,30 @@ import { useState } from "react";
 export function ReservaCita() {
   const { idpsicologo } = useParams();
   // Peticion de datos
-  const { data: [psicologo], loading, error, mensaje } = useFetchData([
-    () => psicologoRequest(idpsicologo)
-  ]);
+  const {
+    data: [psicologo],
+    loading,
+    error,
+    mensaje,
+  } = useFetchData([() => psicologoRequest(idpsicologo)]);
 
   // Estado para días y horarios
   const [diaSeleccionado, setDiaSeleccionado] = useState("");
   const [horarioSeleccionado, setHorarioSeleccionado] = useState("");
 
   // Generar opciones de días y horarios dinámicamente
-  const diasDisponibles = psicologo?.horarios.map((horario) => horario.dia) || [];
-  const horariosPorDia = psicologo?.horarios.reduce((acc, horario) => {
-    acc[horario.dia] = horario.turnos.map(
-      (turno) =>
-        `${formatearHora(turno.hora_inicio)} - ${formatearHora(turno.hora_fin)}`
-    );
-    return acc;
-  }, {}) || {};
+  const diasDisponibles =
+    psicologo?.horarios.map((horario) => horario.dia) || [];
+  const horariosPorDia =
+    psicologo?.horarios.reduce((acc, horario) => {
+      acc[horario.dia] = horario.turnos.map(
+        (turno) =>
+          `${formatearHora(turno.hora_inicio)} - ${formatearHora(
+            turno.hora_fin
+          )}`
+      );
+      return acc;
+    }, {}) || {};
 
   // Manejadores de cambios
   const handleDiaChange = (e) => {
@@ -52,96 +59,108 @@ export function ReservaCita() {
   return (
     <div className={`${styles.fondo}`}>
       <NavBarMental />
-      {loading ? (<div>Cargando ...</div>)
-        : error ? (<h1>{mensaje}</h1>)
-          : (
-      <section>
-        <div className="container-fluid">
-          <div className="row ms-4">
-            <div className="col-12">
-              <NombrePantalla nombre="Reservar cita" />
-              <form>
-                <div className="row mb-3 d-flex justify-content-center">
-                  <InputInfoConLabel propiedad="Nombre" ejemplo={psicologo.nombre} />
-                  <InputInfoConLabel propiedad="Apellidos" ejemplo={psicologo.apellidop + " " + psicologo.apellidom} />
-                </div>
-                <div className="row mb-3 d-flex justify-content-center">
-                  <div className="col-5 ms-5">
-                    <label htmlFor="dia" className="form-label">
-                      Día de atención
-                    </label>
-                    <select
-                      className="form-select w-75"
-                      id="dia"
-                      value={diaSeleccionado}
-                      onChange={handleDiaChange}
-                    >
-                      <option value="">Selecciona un día</option>
-                      {diasDisponibles.map((dia) => (
-                        <option key={dia} value={dia}>
-                          {dia}
-                        </option>
-                      ))}
-                    </select>
+      {loading ? (
+        <div>Cargando ...</div>
+      ) : error ? (
+        <h1>{mensaje}</h1>
+      ) : (
+        <section>
+          <div className="container-fluid">
+            <div className="row ms-4">
+              <div className="col-12">
+                <NombrePantalla nombre="Reservar cita" />
+                <form>
+                  <div className="row mb-3 d-flex justify-content-center">
+                    <InputInfoConLabel
+                      propiedad="Nombre"
+                      ejemplo={psicologo.nombre}
+                    />
+                    <InputInfoConLabel
+                      propiedad="Apellidos"
+                      ejemplo={psicologo.apellidop + " " + psicologo.apellidom}
+                    />
                   </div>
-                  <div className="col-5 ms-5">
-                    <label htmlFor="horario" className="form-label">
-                      Horario disponible
-                    </label>
-                    <select
-                      className="form-select w-75"
-                      id="horario"
-                      value={horarioSeleccionado}
-                      onChange={handleHorarioChange}
-                      disabled={!diaSeleccionado} // Desactivar si no hay día seleccionado
-                    >
-                      <option value="">Selecciona un horario</option>
-                      {diaSeleccionado &&
-                        horariosPorDia[diaSeleccionado]?.map((horario) => (
-                          <option key={horario} value={horario}>
-                            {horario}
+                  <div className="row mb-3 d-flex justify-content-center">
+                    <div className="col-5 ms-5">
+                      <label htmlFor="dia" className="form-label">
+                        Día de atención
+                      </label>
+                      <select
+                        className="form-select w-75"
+                        id="dia"
+                        value={diaSeleccionado}
+                        onChange={handleDiaChange}
+                      >
+                        <option value="">Selecciona un día</option>
+                        {diasDisponibles.map((dia) => (
+                          <option key={dia} value={dia}>
+                            {dia}
                           </option>
                         ))}
-                    </select>
+                      </select>
+                    </div>
+                    <div className="col-5 ms-5">
+                      <label htmlFor="horario" className="form-label">
+                        Horario disponible
+                      </label>
+                      <select
+                        className="form-select w-75"
+                        id="horario"
+                        value={horarioSeleccionado}
+                        onChange={handleHorarioChange}
+                        disabled={!diaSeleccionado} // Desactivar si no hay día seleccionado
+                      >
+                        <option value="">Selecciona un horario</option>
+                        {diaSeleccionado &&
+                          horariosPorDia[diaSeleccionado]?.map((horario) => (
+                            <option key={horario} value={horario}>
+                              {horario}
+                            </option>
+                          ))}
+                      </select>
+                    </div>
                   </div>
-                </div>
-                <div className="row mb-3 d-flex justify-content-center">
-                  <div className="col-8">
-                    <label htmlFor="descripcion" className="form-label">
-                      Motivo
-                    </label>
-                    <textarea
-                      className="form-control"
-                      id="descripcion"
-                      rows="4"
-                    ></textarea>
+                  <div className="row mb-3 d-flex justify-content-center">
+                    <div className="col-8">
+                      <label htmlFor="descripcion" className="form-label">
+                        Motivo
+                      </label>
+                      <textarea
+                        className="form-control"
+                        id="descripcion"
+                        rows="4"
+                      ></textarea>
+                      <ul className="list-group col-5 mt-4 w-25">
+                        <li className="list-group-item">
+                          <input
+                            className="form-check-input me-3"
+                            type="checkbox"
+                            id="secondCheckbox"
+                          />
+                          <label
+                            className="form-check-label"
+                            htmlFor="secondCheckbox"
+                          >
+                            Consulta Online
+                          </label>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                </div>
-                <ul className="list-group col-5">
-                  <li className="list-group-item">
-                    <input
-                      className="form-check-input me-3"
-                      type="checkbox"
-                      id="secondCheckbox"
-                    />
-                    <label className="form-check-label" htmlFor="secondCheckbox">
-                      Consulta Online
-                    </label>
-                  </li>
-                </ul>
-                <div className="row mb-4">
-                  <div className="col-10 d-flex justify-content-end">
-                    <Link to="/metodo-pago">
-                      <BotonAccion nombre="Completar cita" />
-                    </Link>
+
+                  <div className="row mb-4">
+                    <div className="col-10 d-flex justify-content-end">
+                      <Link to="/metodo-pago">
+                        <BotonAccion nombre="Completar cita" />
+                      </Link>
+                    </div>
                   </div>
-                </div>
-              </form>
+                </form>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-        )}
+        </section>
+      )}
     </div>
   );
 }
