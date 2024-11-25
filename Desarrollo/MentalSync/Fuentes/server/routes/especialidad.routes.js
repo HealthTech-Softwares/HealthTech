288cebd41ed8373b3426ed11fa146973dd5e5cd3
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2024-11-24 23:07:14 -0500

    controllers de psicologo

--- Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
+++ Desarrollo/MentalSync/Fuentes/server/routes/especialidad.routes.js
@@ -4,14 +4,16 @@ import {
   getEspecialidades,
   getEspsPsicologo,
   getEspFaltantesPsicologo,
+  updateEspPsicologo,
 } from "../controllers/especialidad.controller.js";
-import { verifyToken, isAdmin, hasRole } from "../libs/auth.middleware.js";
+import { verifyToken, isAdmin, hasRole, isPsicologo } from "../libs/auth.middleware.js";
 
 const router = Router();
 
 router.post("/especialidades/:id", verifyToken, isAdmin, createEspPsicologo); //Asignar especialidad a psicologo
 router.get("/especialidades", verifyToken, hasRole("Administrador", "Paciente"), getEspecialidades); //Ver las especialidades que tiene el psicologo (para el admin)
-router.get("/especialidades/:id", verifyToken, isAdmin, getEspsPsicologo); //Ver las especialidades que tiene un psicologo
-router.get("/especialidades/faltantes/:id", verifyToken, isAdmin, getEspFaltantesPsicologo); //Ver las especialidades que no tiene un psicologo
+router.get("/especialidades/:id", verifyToken, hasRole("Administrador", "Paciente"), getEspsPsicologo); //Ver las especialidades que tiene un psicologo
+router.get("/especialidades/faltantes", verifyToken, isPsicologo, getEspFaltantesPsicologo); //Ver las especialidades que no tiene un psicologo
+router.put("/especialidades", verifyToken, isPsicologo, updateEspPsicologo); //Agregar especialidades del psicologo
 
 export default router;
