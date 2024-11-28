81787c3c19467098d0ef6ce1961d9e9e544a22bd
RichardTorresRivera <rmtr2711@gmail.com>
2024-11-28 01:02:10 -0500

    ver datos estaticos de psico para admin

--- Desarrollo/MentalSync/Fuentes/client/src/routes/admin.routes.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/routes/admin.routes.jsx
@@ -1,14 +1,18 @@
 import { Route } from "react-router-dom";
-import { ListaPsicologos } from "../components/administrador/lista_psicologos/lista_psicologos"
-import { AgregarPsicologo } from "../components/administrador/agregar_psicologo/agregar_psicologo"
-// import { Notificaciones } from "../components/general/notificaciones/notificaciones"
+import { ListaPsicologos } from "../components/administrador/lista_psicologos/lista_psicologos";
+import { AgregarPsicologo } from "../components/administrador/agregar_psicologo/agregar_psicologo";
+import { ModificarPsicoAdmin } from "../components/administrador/modificar_psico/mofificar_psico";
 
 export default function RutasAdmin() {
   return (
     <>
       <Route path="/lista-psicologos" element={<ListaPsicologos />} />
       <Route path="/agregar-psicologo" element={<AgregarPsicologo />} />
+      <Route
+        path="/modificar-datos-psicologo/:idpsicologo"
+        element={<ModificarPsicoAdmin />}
+      />
       {/* <Route path='/notificaciones' element={ <Notificaciones /> } /> */}
     </>
-  )
+  );
 }
