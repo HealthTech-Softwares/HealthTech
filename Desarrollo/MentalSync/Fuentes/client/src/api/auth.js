4b23706e36cd89360802543f62ecd301f5ea705b
RichardTorresRivera <rmtr2711@gmail.com>
2024-11-28 11:52:20 -0500

    Añadiendo token a verify

--- Desarrollo/MentalSync/Fuentes/client/src/api/auth.js
+++ Desarrollo/MentalSync/Fuentes/client/src/api/auth.js
@@ -2,7 +2,7 @@ import axios from "./axios";
 
 export const loginRequest = async (user) => axios.post("/login", user);
 
-export const verifyTokenRequest = async () => axios.get("/verify");
+export const verifyTokenRequest = async (token) => axios.get("/verify", token);
 
 export const registerRequest = async (user) => axios.post("/pacientes", user);
 
