7f9d56994f7a605aa4bdff0d9946fc2b49ff12ca
Alvaro Chavez <135331266+AlvaroChavezCc@users.noreply.github.com>
2025-04-28 14:38:02 -0500

    Mongo

--- Desarrollo/MentalSync/Fuentes/index.js
+++ Desarrollo/MentalSync/Fuentes/index.js
@@ -1,5 +1,8 @@
 import app from "./server/src/app.js";
 import { PORT } from "./server/src/config.js";
+import { connectMongoDB } from "./server/src/dbMongo.js";
+
+await connectMongoDB();
 
 app.listen(PORT, () => {
   console.log(`Servidor Express escuchando en el puerto ${PORT}`);
--- Desarrollo/MentalSync/Fuentes/server/src/config.js
+++ Desarrollo/MentalSync/Fuentes/server/src/config.js
@@ -11,3 +11,4 @@ export const DB_PORT = process.env.DB_PORT;
 export const DB_NAME = process.env.DB_NAME;
 export const DB_USER = process.env.DB_USER;
 export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const MONGODB_URI = process.env.MONGODB_URI;
