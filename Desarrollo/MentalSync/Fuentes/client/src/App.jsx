8538c7c82712f41bbb5ab68b48dbe9e489da6f87
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2025-04-30 18:28:16 +0000

    a ver si funciona

--- Desarrollo/MentalSync/Fuentes/client/src/App.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/App.jsx
@@ -1,14 +1,44 @@
-import { FuncionChat } from './components/general/crear_chat/FuncionChat'
-import { AuthProvider } from './context/AuthContext'
-import Rutas from './routes/index.routes'
+import { useEffect, useContext } from 'react';
+import { FuncionChat } from './components/general/crear_chat/FuncionChat';
+import { AuthProvider, AuthContext } from './context/AuthContext';
+import Rutas from './routes/index.routes';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-function App() {
+function AppContent() {
+  const { setUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-  return (
-    <AuthProvider>
-      <Rutas />
-    </AuthProvider>
-  )
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      axios.get('/api/pacientes/perfil', {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+        .then(res => {
+          setUser(res.data);
+        })
+        .catch(err => {
+          console.log("Token inválido o expirado");
+          localStorage.removeItem("token");
+          navigate('/login');
+        });
+    } else {
+      navigate('/login');
+    }
+  }, []);
+
+  return <Rutas />;
 }
 
-export default App
+function App() {
+  return (
+    <AuthProvider>
+      <AppContent />
+    </AuthProvider>
+  );
+}
+
+export default App;
