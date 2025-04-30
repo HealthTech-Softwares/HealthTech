cc953f9a4d1cdf1def1c9d87e36bb4a2bf4649b3
Miguel Angel Vera Alva <112670367+antidv@users.noreply.github.com>
2025-04-30 18:29:14 +0000

    devolver

--- Desarrollo/MentalSync/Fuentes/client/src/App.jsx
+++ Desarrollo/MentalSync/Fuentes/client/src/App.jsx
@@ -1,44 +1,14 @@
-import { useEffect, useContext } from 'react';
-import { FuncionChat } from './components/general/crear_chat/FuncionChat';
-import { AuthProvider, AuthContext } from './context/AuthContext';
-import Rutas from './routes/index.routes';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-
-function AppContent() {
-  const { setUser } = useContext(AuthContext);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (token) {
-      axios.get('/api/pacientes/perfil', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-        .then(res => {
-          setUser(res.data);
-        })
-        .catch(err => {
-          console.log("Token inválido o expirado");
-          localStorage.removeItem("token");
-          navigate('/login');
-        });
-    } else {
-      navigate('/login');
-    }
-  }, []);
-
-  return <Rutas />;
-}
+import { FuncionChat } from './components/general/crear_chat/FuncionChat'
+import { AuthProvider } from './context/AuthContext'
+import Rutas from './routes/index.routes'
 
 function App() {
+
   return (
     <AuthProvider>
-      <AppContent />
+      <Rutas />
     </AuthProvider>
-  );
+  )
 }
 
-export default App;
+export default App
